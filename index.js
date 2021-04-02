211cd93f6300312c43f223d5235836d4e10f8882
Ahsan Ullah <ahsanullahsunsbd@gmail.com>
2021-04-02 12:41:46 +0600

    finish

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const MongoClient = require("mongodb").MongoClient;
+const ObjectId = require("mongodb").ObjectId;
+
 require("dotenv").config();
 
 const app = express();
@@ -23,22 +25,36 @@ client.connect((err) => {
 	const GroceryCollection = client
 		.db("Grocery-House")
 		.collection("products");
+	const orderDetailsCollection = client
+		.db("Grocery-House")
+		.collection("fullOrderDetails");
 	// perform actions on the collection object
 	console.log("Database Connected");
 
+	// Post New Product:
 	app.post("/addNewProduct", (req, res) => {
 		console.log(req.body);
 		GroceryCollection.insertOne(req.body).then((result) => {
+			res.send(result.insertedCount > 0);
+		});
+	});
+
+	//Post Full Order Details:
+	app.post("/fullOrderDetails", (req, res) => {
+		console.log(req.body);
+		orderDetailsCollection.insertOne(req.body).then((result) => {
 			console.log(result);
 		});
 	});
 
+	//Get all Products:
 	app.get("/allProducts", (req, res) => {
 		GroceryCollection.find({}).toArray((arr, documents) => {
 			res.send(documents);
 		});
 	});
 
+	// Get selected Product:
 	app.get("/selectedProduct/:name", (req, res) => {
 		console.log(req.params.name);
 		GroceryCollection.find({ productName: req.params.name }).toArray(
@@ -46,6 +62,36 @@ client.connect((err) => {
 				res.send(documents[0]);
 			}
 		);
+	});
+
+	// Get Logged In user Order Products:
+	app.get("/my-orders", (req, res) => {
+		console.log(req.query.email);
+		orderDetailsCollection
+			.find({ buyerEmail: req.query.email })
+			.toArray((arr, documents) => {
+				res.send(documents);
+			});
+	});
+
+	// Get Selected Order:
+	app.get("/my-orders/:selectedOrder", (req, res) => {
+		orderDetailsCollection
+			.find({ _id: ObjectId(`${req.params.selectedOrder}`) })
+			.toArray((arr, documents) => {
+				res.send(documents[0]);
+			});
+	});
+
+	// Delete a product:
+	app.delete("/deleteProduct/:id", (req, res) => {
+		const id = ObjectId(req.params.id);
+		console.log(id);
+		GroceryCollection.deleteOne({
+			_id: ObjectId(`${req.params.id}`),
+		}).then((result) => {
+			console.log(result);
+		});
 	});
 
 	// client.close();
